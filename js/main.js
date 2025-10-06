aba0d137cef64ec07b50059ccb0a13793f83898c
rakesh_nakkilla <rakeshnakkilla@gmail.com>
2025-10-06 22:02:02 +0000

    updated locations

--- js/main.js
+++ js/main.js
@@ -63,3 +63,11 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+// Mobile nav toggle
+const toggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+if (toggle) {
+  toggle.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+  });
+}
